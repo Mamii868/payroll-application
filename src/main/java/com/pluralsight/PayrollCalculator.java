fb3343ce39f9ac7009f1fa6a400e6dbb7a6f2d26
Mamii868 <marcmotenii@gmail.com>
2026-04-10 09:43:56 -0500

    Fix overpaying with overtime

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -8,11 +8,9 @@ public class PayrollCalculator {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
-
         String userName = scanner.nextLine();
 
         System.out.print("Enter the amount of hours you worked: ");
-
         float hoursWorked = scanner.nextFloat();
 
         System.out.print("Enter your pay rate: ");
@@ -21,6 +19,7 @@ public class PayrollCalculator {
         double grossPay = payRate * hoursWorked;
 
         if (hoursWorked > 40) {
+            grossPay = payRate * 40;
             float hoursForOvertimePay = hoursWorked - 40;
             double grossOvertimePay = (payRate * 1.5) * hoursForOvertimePay;
             grossPay += grossOvertimePay;
