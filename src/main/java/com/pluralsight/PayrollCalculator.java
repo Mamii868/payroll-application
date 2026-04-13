986ab0cfafa27e9adb7e63b9eb7144f32f74afb8
Mamii868 <marcmotenii@gmail.com>
2026-04-13 14:32:39 -0500

    Refactor calculations to separate method

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -11,21 +11,26 @@ public class PayrollCalculator {
         String userName = scanner.nextLine();
 
         System.out.print("Enter the amount of hours you worked: ");
-        float hoursWorked = scanner.nextFloat();
+        double hoursWorked = scanner.nextFloat();
 
         System.out.print("Enter your pay rate: ");
-        float payRate = scanner.nextFloat();
+        double payRate = scanner.nextFloat();
 
+        double grossPay = calculatePay(payRate, hoursWorked);
+
+        System.out.printf(userName + ", Your gross pay is: $" + "%.2f", grossPay);
+
+    }
+
+    public static double calculatePay(double payRate, double hoursWorked) {
         double grossPay = payRate * hoursWorked;
 
         if (hoursWorked > 40) {
             grossPay = payRate * 40;
-            float hoursForOvertimePay = hoursWorked - 40;
+            double hoursForOvertimePay = hoursWorked - 40;
             double grossOvertimePay = (payRate * 1.5) * hoursForOvertimePay;
             grossPay += grossOvertimePay;
         }
-
-        System.out.printf( userName + ", Your gross pay is: $" + "%.2f", grossPay);
-
+        return grossPay;
     }
 }
